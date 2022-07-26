5b0c696fd6a6b92eaf168c3e251688659e4a3e63
sundharesan11 <sundharesansk11@gmail.com>
2022-07-26 21:58:46 +0530

    deploying app

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+ end
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+root "posts#index"
+end
